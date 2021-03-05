04ef024f19dee6ec95c078e77788d1ebff20eae9
Econn <Econn50@outlook.com>
2021-03-05 11:29:00 +0000

    Added Experimental content json loader

---
+++ LDtkMonogame.ContentPipeline/JsonProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Newtonsoft.Json;
+using TInput = System.String;
+using TOutput = LDtk.Json.LDtkJson;
+
+namespace LDtk.ContentPipeline
+{
+    [ContentProcessor(DisplayName = "Json Processor")]
+    class JsonProcessor : ContentProcessor<TInput, TOutput>
+    {
+        public override TOutput Process(TInput input, ContentProcessorContext context)
+        {
+            context.Logger.LogMessage(input);
+            return JsonConvert.DeserializeObject<TOutput>(File.ReadAllText(input));
+        }
+    }
+}
