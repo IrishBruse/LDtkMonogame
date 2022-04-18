96a308ec0570f18bce980104a31f4a1749ab44d4
IrishBruse <Econn50@outlook.com>
2022-04-18 17:12:02 +0100

    Cleanup misc files

--- pack.bat
+++
@@ -1,9 +0,0 @@
-dotnet build -c Release ./LDtk -f netcoreapp3.1
-dotnet build -c Release ./LDtk -f net5.0
-dotnet build -c Release ./LDtk -f net6.0
-
-dotnet build -c Release ./LDtk.Codegen
-
-dotnet build -c Release ./LDtk.ContentPipeline
-
-dotnet pack -c Release
