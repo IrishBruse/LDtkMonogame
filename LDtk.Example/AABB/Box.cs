c3dc509a33f3d59760bf2d7be69eccaede8fa4d3
IrishBruse <Econn50@outlook.com>
2022-05-04 16:54:02 +0100

    Example: Migrate example back into this repo

---
+++ LDtk.Example/AABB/Box.cs
@@ -0,0 +1,104 @@
+namespace LDtkMonogameExample.AABB;
+
+using System;
+using Microsoft.Xna.Framework;
+
+public class Box
+{
+    public Vector2 Position { get; set; }
+    public Vector2 Size { get; set; }
+    public Vector2 Pivot { get; set; }
+
+    public Vector2 TopLeft => Position - (Size * Pivot);
+    public Vector2 BottomRight => Position + (Size * (Vector2.One - Pivot));
+
+    public Box(Vector2 position, Vector2 size, Vector2 pivot)
+    {
+        Position = position;
+        Size = size;
+        Pivot = pivot;
+    }
+
+    public bool Contains(Vector2 point)
+    {
+        return point.X >= TopLeft.X
+            && point.X <= BottomRight.X
+            && point.Y >= TopLeft.Y
+            && point.Y <= BottomRight.Y;
+    }
+
+    public bool Contains(Box rect)
+    {
+        bool inside = Contains(rect.TopLeft)
+                    || Contains(rect.BottomRight)
+                    || Contains(new Vector2(rect.TopLeft.X, rect.BottomRight.Y))
+                    || Contains(new Vector2(rect.BottomRight.X, rect.TopLeft.Y));
+
+        return inside;
+    }
+
+    public bool RayCast(Vector2 rayOrigin, Vector2 rayDirection, out Vector2 contactPoint, out Vector2 contactNormal, out float hitNear)
+    {
+        hitNear = 0;
+        contactPoint = default;
+        contactNormal = default;
+
+        Vector2 invdir = new(1f / rayDirection.X, 1f / rayDirection.Y);
+
+        Vector2 near = (TopLeft - rayOrigin) * invdir;
+        Vector2 far = (BottomRight - rayOrigin) * invdir;
+
+        if (float.IsNaN(near.X) || float.IsNaN(near.Y) || float.IsNaN(far.X) || float.IsNaN(far.Y))
+        {
+            return false;
+        }
+
+        // Sort near and fars x and y
+        if (near.X > far.X)
+        {
+            (far.X, near.X) = (near.X, far.X);
+        }
+
+        if (near.Y > far.Y)
+        {
+            (far.Y, near.Y) = (near.Y, far.Y);
+        }
+
+        if (near.X > far.Y || near.Y > far.X)
+        {
+            return false;
+        }
+
+        hitNear = MathF.Max(near.X, near.Y);
+        float hitfar = MathF.Min(far.X, far.Y);
+
+        if (hitfar < 0)
+        {
+            return false;
+        }
+
+        contactPoint = rayOrigin + (hitNear * rayDirection);
+
+        contactNormal = near.X > near.Y
+            ? invdir.X < 0 ? new Vector2(1, 0) : new Vector2(-1, 0)
+            : invdir.Y < 0 ? new Vector2(0, 1) : new Vector2(0, -1);
+
+        return true;
+    }
+
+    public bool Cast(Vector2 direction, Box target, out Vector2 contactPoint, out Vector2 contactNormal, out float hitNear, float deltaTime)
+    {
+        contactPoint = default;
+        contactNormal = default;
+        hitNear = float.PositiveInfinity;
+
+        if (direction.X == 0 && direction.Y == 0)
+        {
+            return false;
+        }
+
+        Box expandedTarget = new(target.Position - (Size / 2f), target.Size + Size, target.Pivot);
+
+        return expandedTarget.RayCast(Position, direction * deltaTime, out contactPoint, out contactNormal, out hitNear) && hitNear >= 0f && hitNear < 1f;
+    }
+}
---
+++ LDtk.Example/LDtkMonogameGame.cs
@@ -0,0 +1,242 @@
+﻿namespace LDtkMonogameExample;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Comora;
+using LDtk;
+using LDtk.Renderer;
+using LDtkMonogameExample.Entities;
+using LDtkTypes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class LDtkMonogameGame : Game
+{
+    // LDtk stuff
+    LDtkFile file;
+    LDtkWorld world;
+    LDtkRenderer renderer;
+    readonly List<EnemyEntity> enemies = new();
+    readonly List<BulletEntity> bullets = new();
+    PlayerEntity player;
+    GunEntity gun;
+
+    Camera camera;
+    Texture2D spriteSheet;
+
+    // Monogame Stuff
+    SpriteBatch spriteBatch;
+    readonly GraphicsDeviceManager graphics;
+    float pixelScale = 1f;
+    public static Texture2D Pixel { get; set; }
+
+    public static bool DebugF1 { get; set; }
+    public static bool DebugF2 { get; set; }
+    public static bool DebugF3 { get; set; }
+
+    public bool useContentPipeline;  // requres you to edit the Content.mgcb to enable
+
+    KeyboardState oldKeyboard;
+
+    public LDtkMonogameGame()
+    {
+        graphics = new GraphicsDeviceManager(this);
+
+        if (useContentPipeline)
+        {
+            Content.RootDirectory = "Content";
+        }
+    }
+
+    void MonogameInitialize()
+    {
+        Window.Title = "LDtkMonogame - Shooter";
+
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        Window.AllowUserResizing = true;
+        IsMouseVisible = true;
+        IsFixedTimeStep = false;
+
+        graphics.PreferredBackBufferWidth = 1280;
+        graphics.PreferredBackBufferHeight = 720;
+        graphics.ApplyChanges();
+
+        Window.ClientSizeChanged += (o, e) => pixelScale = Math.Max(GraphicsDevice.Viewport.Height / 180, 1);
+
+        pixelScale = Math.Max(GraphicsDevice.Viewport.Height / 180, 1);
+
+        Pixel = new Texture2D(GraphicsDevice, 1, 1);
+        Pixel.SetData(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF });
+    }
+
+    protected override void Initialize()
+    {
+        MonogameInitialize();
+
+        camera = new Camera(GraphicsDevice);
+        if (useContentPipeline)
+        {
+            renderer = new LDtkRenderer(spriteBatch, Content);
+            file = LDtkFile.FromFile("World", Content);
+            spriteSheet = Content.Load<Texture2D>("Characters");
+        }
+        else
+        {
+            renderer = new LDtkRenderer(spriteBatch);
+            file = LDtkFile.FromFile("Content/World.ldtk");
+            spriteSheet = Texture2D.FromFile(GraphicsDevice, Path.Combine(Path.GetDirectoryName(file.Path), "Characters.png"));
+        }
+
+        world = file.LoadWorld(Guid.Parse("2c81d720-b4d0-11ec-9871-056972512958"));
+
+        foreach (LDtkLevel level in world.Levels)
+        {
+            foreach (Enemy enemy in level.GetEntities<Enemy>())
+            {
+                enemies.Add(new EnemyEntity(enemy, spriteSheet, renderer));
+            }
+
+            // renderer.PrerenderLevel(level);
+        }
+
+        Gun_Pickup gunData = world.GetEntity<Gun_Pickup>();
+        gun = new GunEntity(gunData, spriteSheet, renderer);
+
+        Player playerData = world.GetEntity<Player>();
+        player = new PlayerEntity(playerData, spriteSheet, renderer, gun);
+
+        foreach (ILDtkEntity item in world.Levels[1].GetAllEntities())
+        {
+            switch (item)
+            {
+                case Enemy:
+                Console.WriteLine("Enemy Type");
+                break;
+                case Player:
+                Console.WriteLine("Player Type");
+                break;
+                case Gun_Pickup:
+                Console.WriteLine("Gun_Pickup Type");
+                break;
+                default:
+                break;
+            }
+        }
+
+        player.onShoot += () =>
+        {
+            BulletEntity b = new(spriteSheet, renderer)
+            {
+                Position = player.Position + new Vector2(player.flip ? -23 : 7, -5.5f),
+                flip = player.flip,
+            };
+            bullets.Add(b);
+        };
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        DebugInput(oldKeyboard);
+
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float totalTime = (float)gameTime.TotalGameTime.TotalSeconds;
+
+        camera.Update(gameTime);
+        camera.Position = new Vector2(player.Position.X, 120);
+        camera.Zoom = pixelScale;
+
+        gun.Update(totalTime);
+
+        foreach (LDtkLevel level in world.Levels)
+        {
+            if (level.Contains(player.Position))
+            {
+                player.level = level;
+                break;
+            }
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].Update(deltaTime);
+        }
+
+        for (int i = bullets.Count - 1; i >= 0; i--)
+        {
+            bullets[i].Update(deltaTime);
+
+            for (int j = enemies.Count - 1; j >= 0; j--)
+            {
+                if (bullets[i].collider.Contains(enemies[j].collider))
+                {
+                    bullets.RemoveAt(i);
+                    enemies[j].Kill(deltaTime);
+                    break;
+                }
+            }
+        }
+
+        player.Update(deltaTime, totalTime);
+
+        oldKeyboard = Keyboard.GetState();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        float totalTime = (float)gameTime.TotalGameTime.TotalSeconds;
+
+        GraphicsDevice.Clear(file.BgColor);
+
+        spriteBatch.Begin(camera, SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+        {
+            // // Draw Levels layers
+            // foreach (LDtkLevel level in world)
+            // {
+            //     renderer.RenderPrerenderedLevel(level);
+            // }
+
+            player.Draw(totalTime);
+
+            // Draw Entities
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Draw(totalTime);
+            }
+
+            // Draw bullets
+            for (int i = 0; i < bullets.Count; i++)
+            {
+                bullets[i].Draw();
+            }
+        }
+
+        gun.Draw();
+
+        spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    static void DebugInput(KeyboardState old)
+    {
+        if (old.IsKeyUp(Keys.F1) && Keyboard.GetState().IsKeyDown(Keys.F1))
+        {
+            DebugF1 = !DebugF1;
+        }
+
+        if (old.IsKeyUp(Keys.F2) && Keyboard.GetState().IsKeyDown(Keys.F2))
+        {
+            DebugF2 = !DebugF2;
+        }
+
+        if (old.IsKeyUp(Keys.F3) && Keyboard.GetState().IsKeyDown(Keys.F3))
+        {
+            DebugF3 = !DebugF3;
+        }
+    }
+}
