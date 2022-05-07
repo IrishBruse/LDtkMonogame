8e97127cd38251976ec5c290ef0998148f5d51ef
IrishBruse <Econn50@outlook.com>
2022-05-07 18:29:40 +0100

    Added SerializeOptions

--- LDtk/Utils/Constants.cs
+++ LDtk/Utils/Constants.cs
@@ -1,13 +1,27 @@
 namespace LDtk;
 
-/// <summary>
-/// General Constants used in LDtkMonogame
-/// </summary>
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary> General Constants used in LDtkMonogame </summary>
 public class Constants
 {
-    /// <summary>
-    /// The supported version of ldtk so you are in a newer version any new features
-    /// may not be added yet please create an issue on the github requesting them
-    /// </summary>
+    /// <summary> The supported version of ldtk so you are in a newer version any new features may not be added yet please create an issue on the github requesting them </summary>
     public static readonly string SupportedLDtkVersion = "0.9.3";
+
+    /// <summary> The converter used internally with JsonSerializer.Deserialize(, Constants.SerializeOptions) not needed by the user just use .FromFile instead </summary>
+    public static readonly JsonSerializerOptions SerializeOptions = new()
+    {
+        Converters =
+        {
+            new JsonStringEnumConverter(),
+            new RectangleConverter(),
+            new Vector2Converter(),
+            new ColorConverter(),
+            new PointConverter(),
+            new GuidConverter(),
+        },
+        PropertyNamingPolicy = new LDtkNamingPolicy(),
+        IgnoreNullValues = true,
+    };
 }
