017635a986c4c6bd86ba43e0200da1e8f9514d29
IrishBruse <Econn50@outlook.com>
2023-01-03 16:32:51 +0000

    Hide warning

--- LDtk.Codegen/LDtkJsonFull.cs
+++ LDtk.Codegen/LDtkJsonFull.cs
@@ -1,7 +1,7 @@
 // This file was auto generated, any changes will be lost.
 namespace LDtk.Codegen;
 
-#pragma warning disable IDE1006, CA1711, CA1720, CA1707
+#pragma warning disable IDE1006, CA1711, CA1720, CA1707, CS1591, CA1716
 using System;
 
 using Microsoft.Xna.Framework;
