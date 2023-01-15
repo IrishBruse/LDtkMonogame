fc19a4fe4e158202bebb013a6c79e70c5a4a491f
IrishBruse <Econn50@outlook.com>
2023-01-15 03:00:34 +0000

    Update Constant to 1.2.5

--- LDtk/Constants.cs
+++ LDtk/Constants.cs
@@ -9,7 +9,7 @@ using LDtk.Parsers;
 public class Constants
 {
     /// <summary> The supported version of ldtk so you are in a newer version any new features may not be added yet please create an issue on the github requesting them </summary>
-    public static readonly string SupportedLDtkVersion = "0.9.3";
+    public static readonly string SupportedLDtkVersion = "1.2.5";
 
     /// <summary> The converter used internally with JsonSerializer.Deserialize(, Constants.SerializeOptions) not needed by the user just use .FromFile instead </summary>
     public static readonly JsonSerializerOptions SerializeOptions = new()
