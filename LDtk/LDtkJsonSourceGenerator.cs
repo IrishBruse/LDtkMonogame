b5c6fddfd9cf328b341a9571427bcc615350789d
PixelDough <alworrell19@gmail.com>
2024-04-15 22:14:03 -0500

    Added EntityReference to Json Source Generator, to fix issue with Get Entity functions when EntityReference is a parameter

--- LDtk/LDtkJsonSourceGenerator.cs
+++ LDtk/LDtkJsonSourceGenerator.cs
@@ -5,4 +5,5 @@ using System.Text.Json.Serialization;
 /// <summary> The json source generator for LDtk files. </summary>
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(LDtkFile))]
+[JsonSerializable(typeof(EntityReference))]
 public partial class LDtkJsonSourceGenerator : JsonSerializerContext;
