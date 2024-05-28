5a4bb1764eced86d6c5ca667b174a52f82601ef2
IrishBruse <Econn50@outlook.com>
2024-05-28 19:53:26 +0100

    1.3.2

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,11 @@
-$version="1.3.1"
+$version="1.3.2"
 
 dotnet test
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+Get-ChildItem -Path ./Nuget/ -Include * -File -Recurse | foreach { $_.Delete()}
 
 dotnet pack ./LDtk/LDtk.csproj -c Release -o ./Nuget/ /p:version=$version
 dotnet pack ./LDtk.Codegen/LDtk.Codegen.csproj -c Release -o ./Nuget/ /p:version=$version
