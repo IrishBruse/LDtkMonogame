97e20ccea9209cb254f13505c433c6a29c67994d
Taylor E <taylor.ernst@hotmail.com>
2024-07-02 18:16:07 -0700

    Addressed breaking change in System.Text.Json (.NET 7+) by explicitily telling the source generator all types that we expect to deserialize. See: https://learn.microsoft.com/en-us/dotnet/core/compatibility/serialization/7.0/reflection-fallback#recommended-action

--- LDtk/LDtkJsonSourceGenerator.cs
+++ LDtk/LDtkJsonSourceGenerator.cs
@@ -2,8 +2,27 @@ namespace LDtk;
 
 using System.Text.Json.Serialization;
 
+using Microsoft.Xna.Framework;
+
 /// <summary> The json source generator for LDtk files. </summary>
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(LDtkFile))]
 [JsonSerializable(typeof(EntityReference))]
+[JsonSerializable(typeof(EntityReference[]))]
+[JsonSerializable(typeof(TilesetRectangle))]
+[JsonSerializable(typeof(TilesetRectangle[]))]
+[JsonSerializable(typeof(Color))]
+[JsonSerializable(typeof(Color[]))]
+[JsonSerializable(typeof(Point))]
+[JsonSerializable(typeof(Point[]))]
+[JsonSerializable(typeof(Vector2))]
+[JsonSerializable(typeof(Vector2[]))]
+[JsonSerializable(typeof(bool))]
+[JsonSerializable(typeof(bool[]))]
+[JsonSerializable(typeof(float))]
+[JsonSerializable(typeof(float[]))]
+[JsonSerializable(typeof(int))]
+[JsonSerializable(typeof(int[]))]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(string[]))]
 public partial class LDtkJsonSourceGenerator : JsonSerializerContext;
